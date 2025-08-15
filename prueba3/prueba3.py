e84cff78e9d7fceb18cb7791220deb6b077c3350
Clemente Hermosilla <clemohermosilla@gmail.com>
2025-08-14 20:51:23 -0400

    git ignore al pycache

---
+++ prueba3/prueba3.py
@@ -0,0 +1,4 @@
+class ClaseDos:
+    def __init__(self,nombre: str) -> None:
+        self.nombre = nombre
+        pass
--- pruebamain.py
+++ pruebamain.py
@@ -1,13 +1,5 @@
-from prueba129.pruebanoseque import EstoyProbandoWeas
+from prueba3.prueba3 import ClaseDos
 
-gambling1 = EstoyProbandoWeas("balarto",100)
-gambling2 = EstoyProbandoWeas("nose?",100)
+waw = ClaseDos("pepe")
+print(waw.nombre)
 
-print(gambling1,gambling2)
-gambling1.gambling(4)
-gambling2.gambling(4)
-
-if gambling1.puntos > gambling2.puntos:
-    print("gana 1")
-else:
-    print("xd")
