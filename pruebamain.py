977c55c143b5cbe7068b64f13324a6730f438f4c
Clemente Hermosilla <clemohermosilla@gmail.com>
2025-08-17 14:22:45 -0400

    prueba archivos

--- pruebamain.py
+++ pruebamain.py
@@ -1,7 +1,7 @@
 from prueba3.prueba3 import ClaseDos
 import os
 
-ruta = os.path.join("prueba3","palabras.txt")
+"""ruta = os.path.join("prueba3","palabras.txt")
 waw = ClaseDos(str(input("dame un nombre: ")))
 print(waw.nombre)
 
@@ -21,5 +21,14 @@ with open(ruta,"r") as leer:
 lineas = "".join(lineas)
 lineas = lineas.split(",")
 lineas = " ".join(lineas)
-print(lineas)
+print(lineas)"""
 
+if not os.path.exists("data"):
+    os.makedirs("data")
+nombre = input("nombre archivo? ")+".txt"
+print(nombre)
+ruta = os.path.join("data",nombre)
+with open(ruta,"w") as archivo:
+    lineas = int(input("cuantas lineas quieres escribir? "))
+    for i in range(lineas):
+        archivo.write(input("Escribe: ")+"\n")
